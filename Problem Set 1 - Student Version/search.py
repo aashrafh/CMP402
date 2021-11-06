897675f733f96f87774fef28e9f16b67a95933e6
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-11-07 01:23:09 +0200

    Implement documented DFS search agent

--- Problem Set 1 - Student Version/search.py
+++ Problem Set 1 - Student Version/search.py
@@ -15,47 +15,52 @@ from helpers import utils
 
 
 def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
-    if problem.is_goal(initial_state):  # Check if the initial state is the goal
-        return []   # No actions are needed
-
-    # A FIFO queue to store all the actions of all paths
-    actions = [[]]
     # A FIFO queue to store the next states to explore. initial_state is the only element at the begining
-    frontier = [initial_state]
-    # An empty set to mark/store the visited nodes
-    explored = []
+    frontier = [(initial_state, [])]
+    explored = set()                    # An empty set to mark/store the visited nodes
 
     while frontier:  # while there are more nodes to explore, do:
-        state = frontier.pop(0)  # Choose the shallowest node in the frontier
-        explored.append(state)   # Mark the current state as visited/explored
-        # Choose the shallowest path = shallowest sequence of actions
-        path = actions.pop(0)
+        # Choose the shallowest node in the frontier
+        state, path = frontier.pop(0)
+        if state not in explored:   # If unexplored node
+            # If you reached the goal, then return the path.
+            if problem.is_goal(state):
+                return path
+            # Otherwise, add the current state to the explored set
+            explored.add(state)
 
-        # Loop over all possible actions from the current state
-        for action in problem.get_actions(state):
-            # Get the child node = the successor
-            successor = problem.get_successor(state, action)
-
-            if successor not in explored and successor not in frontier:
-                # Update the current path
-                new_path = list(path)
-                new_path.append(action)
-                actions.append(new_path)
-
-                if problem.is_goal(successor):  # Test if this is the goal
-                    # Append the last action taken
-                    path.append(action)
-                    return path
-                
-                frontier.append(successor) # Update the frontier with the next possible node to explore
+            # and loop over all the next states
+            for action in problem.get_actions(state):
+                successor = problem.get_successor(state, action)
+                # Append the next states to the frontier to get explored
+                frontier.append((successor, path + [action]))
 
     # If there is no solution, return None
-    return None  
+    return None
 
 
 def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
-    # TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
+    # A LIFO stack to store the next states to explore. initial_state is the only element at the begining
+    frontier = [(initial_state, [])]
+    explored = set()                    # An empty set to mark/store the visited nodes
+
+    while frontier:  # while there are more nodes to explore, do:
+        state, path = frontier.pop()    # Choose the deepest node in the frontier
+        if state not in explored:       # If unexplored node
+            # If you reached the goal, then return the path.
+            if problem.is_goal(state):
+                return path
+            # Otherwise, add the current state to the explored set
+            explored.add(state)
+
+            # and loop over all the next states
+            for action in problem.get_actions(state):
+                successor = problem.get_successor(state, action)
+                # Append the next states to the frontier to get explored
+                frontier.append((successor, path + [action]))
+
+    # If there is no solution, return None
+    return None
 
 
 def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
