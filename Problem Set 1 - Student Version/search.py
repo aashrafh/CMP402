59527eb6161fb054aec534f257d8e67100327c90
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-11-07 00:01:10 +0200

    Document the BFS solution

--- Problem Set 1 - Student Version/search.py
+++ Problem Set 1 - Student Version/search.py
@@ -15,33 +15,42 @@ from helpers import utils
 
 
 def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
-    if problem.is_goal(initial_state):
-        return []
+    if problem.is_goal(initial_state):  # Check if the initial state is the goal
+        return []   # No actions are needed
 
+    # A FIFO queue to store all the actions of all paths
     actions = [[]]
+    # A FIFO queue to store the next states to explore. initial_state is the only element at the begining
     frontier = [initial_state]
+    # An empty set to mark/store the visited nodes
     explored = []
 
-    while frontier:
-        state = frontier.pop(0)
-        explored.append(state)
+    while frontier:  # while there are more nodes to explore, do:
+        state = frontier.pop(0)  # Choose the shallowest node in the frontier
+        explored.append(state)   # Mark the current state as visited/explored
+        # Choose the shallowest path = shallowest sequence of actions
         path = actions.pop(0)
 
+        # Loop over all possible actions from the current state
         for action in problem.get_actions(state):
+            # Get the child node = the successor
             successor = problem.get_successor(state, action)
 
             if successor not in explored and successor not in frontier:
+                # Update the current path
                 new_path = list(path)
                 new_path.append(action)
                 actions.append(new_path)
 
-                if problem.is_goal(successor):
+                if problem.is_goal(successor):  # Test if this is the goal
+                    # Append the last action taken
                     path.append(action)
                     return path
+                
+                frontier.append(successor) # Update the frontier with the next possible node to explore
 
-                frontier.append(successor)
-
-    return None
+    # If there is no solution, return None
+    return None  
 
 
 def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
