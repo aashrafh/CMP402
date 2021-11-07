8cd3bcab909d88403e0fbfbce30191dbf911647e
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-11-07 02:32:07 +0200

    Implement documented UCS search agent

--- Problem Set 1 - Student Version/search.py
+++ Problem Set 1 - Student Version/search.py
@@ -2,6 +2,9 @@ from os import stat
 from problem import HeuristicFunction, Problem, S, A, Solution
 from collections import deque
 from helpers import utils
+from queue import PriorityQueue
+import heapq
+
 
 # TODO: Import any modules or write any helper functions you want to use
 
@@ -64,8 +67,31 @@ def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
 
 
 def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
-    # TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
+    # A FIFO priority queue to store the next states to explore.
+    # initial_state is the only element at the begining with path cost  = 0
+    # Each entry consists of (cumulative_path_cost, (node_string_representation, node), path)
+    frontier = [(0, (initial_state.__str__(), initial_state), [])]
+    explored = set()    # An empty set to mark/store the visited nodes
+
+    while frontier:  # while there are more nodes to explore, do:
+        # Choose the shallowest node in the frontier with the least path cost
+        cost, state, path = heapq.heappop(frontier)
+        if state not in explored:       # If unexplored node
+            # If you reached the goal, then return the path.
+            if problem.is_goal(state[1]):
+                return path
+            # Otherwise, add the current state to the explored set
+            explored.add(state)
+
+            # and loop over all the next states
+            for action in problem.get_actions(state[1]):
+                successor = problem.get_successor(state[1], action)
+                # Append the next states to the frontier to get explored and ordered prior to it's cumulative path cost
+                heapq.heappush(frontier, (cost + problem.get_cost(
+                    state[1], action), (successor.__str__(), successor), path + [action]))
+
+    # If there is no solution, return None
+    return None
 
 
 def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
