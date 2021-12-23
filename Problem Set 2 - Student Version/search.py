6951f571e6a3d42d7e1b2363e4571359cf2d363b
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-12-24 01:26:33 +0200

    Implement alpha-beta pruning, mover ordering, and expectimax

--- Problem Set 2 - Student Version/search.py
+++ Problem Set 2 - Student Version/search.py
@@ -31,21 +31,27 @@ def greedy(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: 
 
 
 def minimax(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
-    agent = game.get_turn(state)
+    agent = game.get_turn(state)   # Get the current player
 
+    # Check if you're in a terminal state, then no action is needed
     terminal, values = game.is_terminal(state)
     if terminal:
         return values[0], None
 
+    # If you reached the maximum depth,
+    # use a heuristic to evaluate the value of the terminla state and no extra actions is needed
     if max_depth == 0:
         return heuristic(game, state, 0), None
 
+    # Get all children of the current node combined with its action
     actions_states = [(action, game.get_successor(state, action))
                       for action in game.get_actions(state)]
     if agent == 0:  # Max player
+        # Get the best action that achieves the best/maximum value
         value, _, action = max((minimax(game, state, heuristic, max_depth-1)[
                                0], -index, action) for index, (action, state) in enumerate(actions_states))
     else:           # Min player
+        # Get the best action that achieves the best/minimum value
         value, _, action = min((minimax(game, state, heuristic, max_depth-1)[
                                0], index, action) for index, (action, state) in enumerate(actions_states))
     return value, action
@@ -53,21 +59,117 @@ def minimax(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth:
 
 # Apply Alpha Beta pruning and return the tree value and the best action
 
+def _alphabeta(game, state, heuristic, max_depth, alpha, beta, sort=False):
+    agent = game.get_turn(state)  # Get the current player
+
+    # Check if you're in a terminal state, then no action is needed
+    terminal, values = game.is_terminal(state)
+    if terminal:
+        return values[0], None
+
+    # If you reached the maximum depth,
+    # use a heuristic to evaluate the value of the terminla state and no extra actions is needed
+    if max_depth == 0:
+        return heuristic(game, state, 0), None
+
+    # Get all children of the current node combined with its action
+    actions_states = [(action, game.get_successor(state, action))
+                      for action in game.get_actions(state)]
+
+    # In case of alpha-beta pruning with move ordering, you need to sort all possible actions/states/nodes
+    # depending on the evaluated heuristic
+    if sort:
+        actions_states = sorted(
+            actions_states, key=lambda item: -heuristic(game, item[1], agent))
+
+    if agent == 0:  # Max player
+        results = []
+        value, action = -math.inf, None
+        # Get the best action that achieves the best/maximum value
+        for index, (action, state) in enumerate(actions_states):
+            cur_value = _alphabeta(game, state, heuristic,
+                                   max_depth - 1, alpha, beta, sort)[0]
+
+            results.append((cur_value, -index, action))
+            value, _, action = max(results)  # Maximum value for the MAX player
+
+            # If the current value is greater than or equal to beta,
+            # prune the search and return the best value with best action
+            if value >= beta:
+                return value, action
+
+            # Update alpha with maximum possible value
+            alpha = max(alpha, value)
+
+    else:           # Min player
+        results = []
+        value, action = math.inf, None
+        # Get the best action that achieves the best/minimum value
+        for index, (action, state) in enumerate(actions_states):
+            cur_value = _alphabeta(game, state, heuristic,
+                                   max_depth - 1, alpha, beta, sort)[0]
+
+            results.append((cur_value, -index, action))
+            value, _, action = min(results)  # Minimum value for the MIN player
+
+            # If the current value is less than or equal to alpha,
+            # prune the search and return the best value with best action
+            if value <= alpha:
+                return value, action
+
+            # Update beta with minimum possible value
+            beta = min(beta, value)
+
+    return value, action
+
 
 def alphabeta(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
-    # TODO: ADD YOUR CODE HERE
-    NotImplemented()
+    return _alphabeta(game, state, heuristic, max_depth, -math.inf, math.inf)
 
 # Apply Alpha Beta pruning with move ordering and return the tree value and the best action
 
 
 def alphabeta_with_move_ordering(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
-    # TODO: ADD YOUR CODE HERE
-    NotImplemented()
+    return _alphabeta(game, state, heuristic, max_depth, -math.inf, math.inf, True)
 
 # Apply Expectimax search and return the tree value and the best action
 
 
 def expectimax(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
-    # TODO: ADD YOUR CODE HERE
-    NotImplemented()
+    def _expectimax(game, state, heuristic, max_depth, chance) -> Tuple[float, A]:
+        agent = game.get_turn(state)  # Get the current player
+
+        # Check if you're in a terminal state, then no action is needed
+        terminal, values = game.is_terminal(state)
+        if terminal:
+            return values[0], None
+
+        # If you reached the maximum depth,
+        # use a heuristic to evaluate the value of the terminla state and no extra actions is needed
+        if max_depth == 0:
+            return heuristic(game, state, 0), None
+
+        # Get all children of the current node combined with its action
+        actions_states = [(action, game.get_successor(state, action))
+                          for action in game.get_actions(state)]
+
+        if chance and agent != 0:  # Chance Node
+            _sum = 0
+            _num = len(actions_states)
+            # Get the average value of all children for the current node
+            for action, state in actions_states:
+                value, action = _expectimax(
+                    game, state, heuristic, max_depth-1, False)
+                _sum += value
+            value = _sum / _num
+        elif agent == 0:  # Max player
+            # Get the best action that achieves the best/maximum value
+            value, _, action = max((_expectimax(game, state, heuristic, max_depth-1, True)[0], -index, action)
+                                   for index, (action, state) in enumerate(actions_states))
+        else:           # Min player
+            # Get the best action that achieves the best/minimum value
+            value, _, action = min((_expectimax(game, state, heuristic, max_depth-1, True)[0], index, action)
+                                   for index, (action, state) in enumerate(actions_states))
+        return value, action
+
+    return _expectimax(game, state, heuristic, max_depth, False)
