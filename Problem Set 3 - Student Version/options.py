66817f3eb0c3166faded116e3194fc25b4eb2320
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-01-06 00:42:11 +0200

    Comment value iteration and parameter selection

--- Problem Set 3 - Student Version/options.py
+++ Problem Set 3 - Student Version/options.py
@@ -1,6 +1,8 @@
 # This file contains the options that you should modify to solve Question 2
 
 def question2_1():
+    # Seek the near terminal state (reward +1) via the short dangerous path (moving besides the row of -10 state).
+    # Decrease the noise so the agent moves faster, and less discount factor so do not care much about the reward
     return {
         "noise": 0.002,
         "discount_factor": 0.09,
@@ -9,6 +11,8 @@ def question2_1():
 
 
 def question2_2():
+    # Seek the near terminal state (reward +1) via the long safe path (moving away from the row of -10 state).
+    # Increase the noise so the agent takes thet longer path, and increase the discount factor more than the Q1
     return {
         "noise": 0.5,
         "discount_factor": 0.67,
@@ -17,6 +21,8 @@ def question2_2():
 
 
 def question2_3():
+    # Seek the far terminal state (reward +10) via the short dangerous path (moving besides the row of -10 state).
+    # Same as Q1 but with higher discount factor to take care more about the reward
     return {
         "noise": 0.002,
         "discount_factor": 0.9,
@@ -25,6 +31,8 @@ def question2_3():
 
 
 def question2_4():
+    # Seek the far terminal state (reward +10) via the long safe path (moving away from the row of -10 state).
+    # Increase the noise so the agent takes thet longer path and increase the reward along with the discount factor.
     return {
         "noise": 0.2,
         "discount_factor": 1,
@@ -33,6 +41,9 @@ def question2_4():
 
 
 def question2_5():
+    # Avoid any terminal state and keep the episode going on forever.
+    # Increase the reward of going to non-terminal states and decrease the noise to increase probability of
+    # going to non-terminal states.
     return {
         "noise": 0.02,
         "discount_factor": 0.99,
@@ -41,6 +52,8 @@ def question2_5():
 
 
 def question2_6():
+    # Seek any terminal state (even ones with the -10 penalty) and try to end the episode in the shortest time possible.
+    # Decrease the reward very much so the agent doesn't care about the non-terminal states
     return {
         "noise": 0.2,
         "discount_factor": 0.99,
--- Problem Set 3 - Student Version/value_iteration.py
+++ Problem Set 3 - Student Version/value_iteration.py
@@ -22,17 +22,23 @@ class ValueIterationAgent(Agent[S, A]):
         self.discount_factor = discount_factor
 
     def _compute_bellman(self, state, actions):
-        values = []
-        for action in actions:
+        values = []    # Initialize a list to store value of each action
+        for action in actions:  # Loop over all possible actions
+            # Get all the possible next states from current state taking current action
             successors = self.mdp.get_successor(state, action)
             value = 0
+            # Loop over each next state and its probability
             for next_state, p in successors.items():
+                # Get the reward of going to the next state taking current action from current state
                 reward = self.mdp.get_reward(state, action, next_state)
+                # accumulate the value calculated from Bellman Equation
                 value += p * (reward + self.discount_factor *
                               self.utilities[str(next_state)])
+            # Append the current action with its value to the list
             values.append((value, action))
         # Sort the values depeding on the utility. This is a stable sort
         values.sort(key=lambda item: (-item[0], item[1]))
+        # Return the first action that has the maximum utility
         return values[0]
 
     # Given a state, compute its utility using the bellman equation
@@ -40,6 +46,7 @@ class ValueIterationAgent(Agent[S, A]):
     def compute_bellman(self, state: S) -> float:
         if self.mdp.is_terminal(state):  # if the state is terminal, return 0
             return 0
+        # Compute Bellman's Equation and return the utility only
         return self._compute_bellman(state, self.mdp.get_actions(state))[0]
 
     # This function applies value iteration starting from the current utilities stored in the agent and stores the new utilities in the agent
@@ -47,16 +54,21 @@ class ValueIterationAgent(Agent[S, A]):
     # In other words, calling train(M) followed by train(N) is equivalent to just calling train(N+M)
     def train(self, iterations: int = 1):
         for i in range(iterations):
+            # Initialize a dictionary for the current iteration to store utility of each state
             utilities = {str(state): 0 for state in self.mdp.get_states()}
+            # Loop over all possible states in the MDP
             for state in self.mdp.get_states():
+                # Calculate the utility using Bellman's Equation
                 utilities[str(state)] = self.compute_bellman(state)
+            # Update the Agent's utilities with the  new calculated ones
             self.utilities = utilities.copy()
 
     # Given an environment and a state, return the best action as guided by the learned utilities and the MDP
     # If the state is terminal, return None
     def act(self, env: Environment[S, A], state: S) -> A:
-        if self.mdp.is_terminal(state):
+        if self.mdp.is_terminal(state):  # If the state is terminal, return None
             return None
+        # Compute Bellman's Equation and return the action only
         return self._compute_bellman(state, env.actions())[1]
 
     # Save the utilities to a json file
